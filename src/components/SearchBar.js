5f492b1d2e3cbee9e63a430c75034fcd3837ab42
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-02-23 12:21:25 -0500

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={props.radio1Checked} onChange={(e) => props.handleSortStocks(e.target.value)}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={props.radio2Checked} onChange={(e) => props.handleSortStocks(e.target.value)}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={(e) => props.handleFilterStocks(e.target.value)}>
+          <option value="All">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,140 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    allStocks: [],
+    shownStocks: [],
+    portfolio: [],
+    sortByName: false,
+    sortByPrice: false,
+    filter: false,
+    filterBy: "All",
+    radio1Checked: false,
+    radio2Checked: false
+  }
+
+  // initial fetching of all stocks
+  componentDidMount() {
+    this.fetchStocks()
+  }
+  fetchStocks = () => {
+    fetch('http://localhost:3000/stocks')
+    .then( r => r.json() )
+    .then( stocks => {
+      this.setState({
+        allStocks: stocks,
+        shownStocks: stocks
+      })
+    })
+  }
+
+  // adds a stock to a user's portfolio
+  handleAddToPortfolio = (stock) => {
+    let clickedStock = stock
+    this.setState({
+      portfolio: [...this.state.portfolio, clickedStock]
+    })
+  }
+
+  // removes a stock from a user's portfolio
+  handleRemoveFromPortfolio = (stock) => {
+    let clickedStock = stock
+    let portfolioArr = this.state.portfolio
+    let newPortfolioArr = portfolioArr.filter( stock => stock !== clickedStock )
+    this.setState({
+      portfolio: newPortfolioArr
+    })
+  }
+
+  // sorts stocks
+  handleSortStocks = (sortType) => {
+    if (sortType === "Alphabetically") {
+      this.setState({
+        sortByName: !this.state.sortByName
+      })
+      let sortedStocks = this.state.shownStocks.slice()
+      sortedStocks = sortedStocks.sort((a, b) => this.sortByName(a.name, b.name))
+      this.setState({
+        shownStocks: sortedStocks,
+        radio1Checked: true,
+        radio2Checked: false
+      })
+    } else if (sortType === "Price") {
+      this.setState({
+        sortByPrice: !this.state.sortByPrice
+      })
+      let sortedStocks = this.state.shownStocks.slice()
+      sortedStocks = sortedStocks.sort((a, b) => a.price - b.price )
+      this.setState({
+        shownStocks: sortedStocks,
+        radio1Checked: false,
+        radio2Checked: true
+      })
+    }
+  }
+    // sort by name helper
+  sortByName = (a, b) => {
+    let nameA = a.toUpperCase()
+    let nameB = b.toUpperCase()
+    if (nameA < nameB) {
+      return -1
+    }
+    if (nameA > nameB) {
+      return 1
+    }
+  }
+
+  // filters stocks
+  handleFilterStocks = (filterType) => {
+    let newFilter;
+
+    switch (filterType) {
+      case "All":
+        newFilter = "All"
+        break;
+      case "Tech":
+        newFilter = "Tech"
+        break;
+      case "Sportswear":
+        newFilter = "Sportswear"
+        break;
+      case "Finance":
+        newFilter = "Finance"
+        break;
+      default:
+        console.log("oops")
+    }
+    if (newFilter === "All") {
+      this.setState({
+        shownStocks: this.state.allStocks,
+        radio1Checked: false,
+        radio2Checked: false
+      })
+    } else {
+      this.setState({
+        shownStocks: this.state.allStocks.filter( stock => stock.type === newFilter ),
+        radio1Checked: false,
+        radio2Checked: false
+      })
+    }
+  }
+
+  // render
   render() {
     return (
       <div>
-        <SearchBar/>
+
+        <SearchBar handleSortStocks={this.handleSortStocks} handleFilterStocks={this.handleFilterStocks} radio1Checked={this.state.radio1Checked} radio2Checked={this.state.radio2Checked} />
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer shownStocks={this.state.shownStocks} handleAddToPortfolio={this.handleAddToPortfolio} />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer portfolio={this.state.portfolio} handleRemoveFromPortfolio={this.handleRemoveFromPortfolio} />
 
             </div>
           </div>
