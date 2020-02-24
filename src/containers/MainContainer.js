c1318ee757bf5a25986a8211e6aa373bb418aae1
tdonovan79 <tdonovan79@gmail.com>
2020-02-24 17:41:05 -0500

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,58 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    allStocks: [],
+    boughtStocks: [],
+    shownStocks: []
+  }
+  //fetch data when component mounts
+  componentDidMount(){
+    fetch('http://localhost:3000/stocks')
+    .then(res=>res.json())
+    .then(data=>{
+      this.setState({
+        allStocks: data,
+        shownStocks: data
+      })
+    })
+  }
+  //add stock to bought stocks when listed stock is clicked
+  handleStockBuy = (thisStock) => {
+    this.setState({
+      boughtStocks: [...this.state.boughtStocks, thisStock]
+    })
+  }
+  //sort stocks alphabetically
+  sortStocksAlpha = () => {
+    let newArray = this.state.shownStocks.slice()
+    newArray.sort((a,b) => a.ticker > b.ticker ? 1 : -1)
+    this.setState({
+      shownStocks: newArray
+    })
+  }
+  //sort stocks by price
+  sortStocksPrice = () => {
+    let newArray = this.state.shownStocks.slice()
+    newArray.sort((a,b) => a.price > b.ticker ? 1 : -1)
+    this.setState({
+      shownStocks: newArray
+    })
+  }
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sortStocksAlpha={this.sortStocksAlpha} sortStocksPrice={this.sortStocksPrice}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer allStocks={this.state.shownStocks} handleStockBuy={this.handleStockBuy}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer boughtStocks={this.state.boughtStocks}/>
 
             </div>
           </div>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -7,9 +7,11 @@ class PortfolioContainer extends Component {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+        {
+          this.props.boughtStocks.map(thisStock => {
+            return <Stock thisStock={thisStock} key={thisStock.ticker} />
+          })
+        }
       </div>
     );
   }
