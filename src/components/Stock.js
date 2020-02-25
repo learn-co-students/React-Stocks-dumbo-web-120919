62877be50c999c5fa8f51715be0d97705a4e48cf
Annie Liao <liaowow@gmail.com>
2020-02-24 20:04:58 -0500

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,19 +1,25 @@
 import React from 'react'
 
-const Stock = () => (
+const Stock = (props) => (
+
+  // function handleStockClick() {
+  //   props.addToPortfolio(props.stock)
+  // }
+
   <div>
 
-    <div className="card">
+    <div className="card" onClick={() => props.handleStock(props.stock)}>
       <div className="card-body">
         <h5 className="card-title">{
             //Company Name
+            props.stock.name
           }</h5>
         <p className="card-text">{
             //ticker: stock price
+            `${props.stock.ticker}: ${props.stock.price}`
           }</p>
       </div>
     </div>
-
 
   </div>
 );
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,95 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks: [],
+    displayStocks: [],
+    portfolioStocks: []
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/stocks")
+    .then(r => r.json())
+    .then(stocks => {
+      this.setState({
+        stocks,
+        displayStocks: stocks
+      })
+    })
+  }
+
+  addToPortfolio = (stock) => {
+    this.setState({
+      portfolioStocks: [...this.state.portfolioStocks, stock]
+    })
+  }
+
+  removeFromPortfolio = (stockToRemove) => {
+    this.setState({
+      portfolioStocks: this.state.portfolioStocks.filter(stock => stock !== stockToRemove)
+    })
+  }
+
+  sortStock = (selectedType) => {
+    let sortedStock = []
+    if (selectedType === "Alphabetically") {
+      sortedStock = this.state.displayStocks.sort((a, b) => a.name.localeCompare(b.name))
+    }
+
+    if (selectedType === "Price") {
+      sortedStock = this.state.displayStocks.sort((a, b) => a.price - b.price)
+    }
+
+    this.setState({
+      displayStocks: sortedStock
+    })
+  }
+
+  filterStock = (filteredType) => {
+    
+    if (filteredType !== "All") {
+      this.setState({
+        displayStocks: this.state.stocks.filter(stock => stock.type === filteredType)
+      })
+    } else {
+      this.setState({
+        displayStocks: this.state.stock
+      })
+    }
+
+    // let filteredStock = []
+
+    // if (filteredType === "Tech") {
+    //   filteredStock = this.state.stocks.filter(stock => stock.type === filteredType)
+    // }
+
+    // if (filteredType === "Finance") {
+    //   filteredStock = this.state.stocks.filter(stock => stock.type === filteredType)
+    // }
+
+    // if (filteredType === "Sportswear") {
+    //   filteredStock = this.state.stocks.filter(stock => stock.type === filteredType)
+    // }
+
+    // this.setState({
+    //   displayStocks: filteredStock
+    // })
+  }
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sortStock={this.sortStock} filterStock={this.filterStock}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.displayStocks} handleStock={this.addToPortfolio}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={this.state.portfolioStocks} handleStock={this.removeFromPortfolio}/>
 
             </div>
           </div>
